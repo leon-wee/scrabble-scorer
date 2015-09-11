2f29f21c6f26008cc666b8fb806d688753bbe402
Ptolemy <ptolemybarnes@gmail.com>
2015-09-11 13:31:17 +0100

    Provides expectation

--- spec/scrabble_scorer_spec.rb
+++ spec/scrabble_scorer_spec.rb
@@ -3,7 +3,7 @@ require './lib/scrabble_scorer'
 describe ScrabbleScorer do 
 
   it 'finds the highest scoring word(s)' do
-    expect(ScrabbleScorer.call).to eq '???'
+    expect(ScrabbleScorer.call).to eq "RAZZAMATAZZES"
   end
 
 end
