a77006bf4719f9b3f7ebacc709994f8b7e60234f
leon-wee <leonwee27@gmail.com>
2015-09-12 14:36:16 +0700

    Finished scrabble scorer

--- lib/scrabble_scorer.rb
+++ lib/scrabble_scorer.rb
@@ -1,13 +1,18 @@
+require 'yaml'
+
 class ScrabbleScorer
 
 
-  def score(string)
-    hash = { 'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1, 'N' => 1, 'S' => 1, 'T' => 1, 'R' => 1, 'D' => 2, 'G' => 2, 'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3, 'Q'=>10,'Z'=>10,'J'=>8,'X'=>8,'K'=>5,'F'=>4,'H'=>4,'V'=>4,'W'=>4,'Y'=>4 }
+  def self.score(string)
+    hash = { 'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1, 'L' => 1, 'N' => 1, 'S' => 1, 'T' => 1, 'R' => 1, 'D' => 2, 'G' => 2, 'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3, 'Q' => 10, 'Z' => 10, 'J' => 8, 'X' => 8, 'K' => 5, 'F' => 4, 'H' => 4, 'V' => 4, 'W' => 4, 'Y' => 4 }
     string.split('').map { |x| hash[x.upcase] }.inject(:+)
   end
 
   def self.call
-    File.open("./word_list.txt")
+    array = YAML.load(File.open("word_list.txt"))
+    scores = array.map { |x| ScrabbleScorer.score(x) }
+    index = scores.index(scores.max)
+    array[index]
   end
 
 end
